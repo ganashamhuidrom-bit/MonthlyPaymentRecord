be63c35a32e4dff0925ffece30465f1f45ae36dd
ganashamhuidrom-bit <ganashamhuidrom@gmail.com>
2026-06-04 13:16:25 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/monthlypayment/MainActivity.java
+++ app/src/main/java/com/example/monthlypayment/MainActivity.java
@@ -1,10 +1,15 @@
-
 package com.example.monthlypayment;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity
+        extends AppCompatActivity {
+
+    Button btnAdd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +17,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+
+        btnAdd = findViewById(R.id.btnAdd);
+
+        btnAdd.setOnClickListener(v -> {
+
+            Intent intent =
+                new Intent(
+                    MainActivity.this,
+                    AddCustomerActivity.class
+                );
+
+            startActivity(intent);
+        });
     }
-}
+        }
